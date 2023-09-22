97a8b330d0fecca65c8eddb8122b623ec6e959dd
Divider <it-cio@mail.ru>
2023-09-22 14:23:49 +0300

    Tune app to work with all jpegs

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from PIL import Image
 
 
@@ -9,11 +11,12 @@ def insert_logo(input_photo_path, output_photo_path, logo_path, position):
     transparent = Image.new('RGB', (width, height), (0, 0, 0, 0))
     transparent.paste(base_image, (0, 0))
     transparent.paste(logo, position, mask=logo)
-    transparent.show()
+    # transparent.show()
     transparent.save(output_photo_path)
 
 
 if __name__ == '__main__':
-    img = 'photo.jpg'
-    insert_logo(img, 'photo_logo.jpg',
-                'logo.png', position=(0, 50))
+    for file in os.listdir():
+        if file.lower().endswith('.jpg'):
+            insert_logo(file, f'logo/logo_{file}',
+                        'logo/logo.png', position=(0, 50))
