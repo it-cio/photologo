dbb5b10f95a4b268ad99a8fdb5da29c2e4800ff0
Divider <it-cio@mail.ru>
2024-09-18 17:09:45 +0300

    Add auto resize logo method

--- app.py
+++ app.py
@@ -3,14 +3,23 @@ import os
 from PIL import Image
 
 
-def insert_logo(input_photo_path, output_photo_path, logo_path, position):
+def insert_logo(input_photo_path, output_photo_path, logo_path):
     base_image = Image.open(input_photo_path)
     logo = Image.open(logo_path)
     width, height = base_image.size
 
     transparent = Image.new('RGB', (width, height), (0, 0, 0, 0))
     transparent.paste(base_image, (0, 0))
-    transparent.paste(logo, position, mask=logo)
+
+    fixed_width = int(base_image.width / 100 * 15)
+    width_percent = fixed_width / logo.width * 100
+    height_size = int(logo.height / 100 * width_percent)
+    logo_resize = logo.resize((fixed_width, height_size))
+
+    indent = int(base_image.height / 100 * 5)
+
+    position = (base_image.width-logo_resize.width-indent, base_image.height-logo_resize.height-indent)
+    transparent.paste(logo_resize, position, mask=logo_resize)
     # transparent.show()
     transparent.save(output_photo_path)
 
@@ -21,4 +30,4 @@ if __name__ == '__main__':
             if not os.path.isdir('modified'):
                 os.mkdir('modified')
             insert_logo(file, f'modified/logo_{file}',
-                        'logo/logo.png', position=(70, 150))
+                        'logo/logo.png')
